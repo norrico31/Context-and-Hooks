1e89eadc5dcc212020ba5496efbfe16ee7c2e330
Norrico Gerald Biason <geraldbiasonjob31@gmail.com>
2020-04-05 13:54:45 +0800

    ThemeToggle.js added and connected to ThemeContext.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import BookList from './components/BookList';
 import ThemeContextProvider from './contexts/ThemeContext';
+import ThemeToggle from './components/ThemeToggle';
 
 function App() {
   return (
-    <div className="App">
-      <ThemeContextProvider>
-        <Navbar />
-        <BookList />
-      </ThemeContextProvider>
-    </div>
+      <div className="App">
+          <ThemeContextProvider>
+              <Navbar />
+              <BookList />
+              <ThemeToggle />
+          </ThemeContextProvider>
+      </div>
   );
 }
 
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -17,9 +17,14 @@ class ThemeContextProvider extends Component {
         }
     }
 
+    // toggle button in components ThemeToggle.js
+    toggleTheme = () => {
+        this.setState({ isLightTheme: !this.state.isLightTheme });
+    }
+
     render() {
         return (
-            <ThemeContext.Provider value={{...this.state}}>
+            <ThemeContext.Provider value={{ ...this.state, toggleTheme: this.toggleTheme }}>
                 {this.props.children}
             </ThemeContext.Provider>
         )
