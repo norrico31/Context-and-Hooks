1e19713bd21999716709b67b8f8d2bef7693cded
Norrico Gerald Biason <geraldbiasonjob31@gmail.com>
2020-04-06 22:48:42 +0800

    added useContext in Booklist.js and it is freaking awesome

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,34 +1,59 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react'; // Added useContext
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-    static contextType = ThemeContext;
+// class BookList extends Component {
+//     static contextType = ThemeContext;
 
-    render() {
+//     render() {
 
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark ;
+//         const { isLightTheme, light, dark } = this.context;
+//         const theme = isLightTheme ? light : dark ;
 
-        const booklistStyle = {
-            color: theme.syntax,
-            background: theme.bg
-        };
-        const liStyle = {
-            background: theme.ui
-        };
+//         const booklistStyle = {
+//             color: theme.syntax,
+//             background: theme.bg
+//         };
+//         const liStyle = {
+//             background: theme.ui
+//         };
         
-        return (
-            <div className="book-list" style={ booklistStyle }>
-                <ul>
-                    <li style={ liStyle }>The way of kings</li>
-                    <li style={ liStyle }>The name of the wind</li>
-                    <li style={ liStyle }>The final empire</li>
-                </ul>
-            </div>
-        )
-    }
-    
+//         return (
+//             <div className="book-list" style={ booklistStyle }>
+//                 <ul>
+//                     <li style={ liStyle }>The way of kings</li>
+//                     <li style={ liStyle }>The name of the wind</li>
+//                     <li style={ liStyle }>The final empire</li>
+//                 </ul>
+//             </div>
+//         )
+//     }
+// }
 
+
+// useContext
+const BookList = () => {
+
+    // useContext();
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark ;
+
+    const booklistStyle = {
+        color: theme.syntax,
+        background: theme.bg
+    };
+    const liStyle = {
+        background: theme.ui
+    };
+
+    return (
+        <div className="book-list" style={ booklistStyle }>
+            <ul>
+                <li style={ liStyle }>The way of kings</li>
+                <li style={ liStyle }>The name of the wind</li>
+                <li style={ liStyle }>The final empire</li>
+            </ul>
+        </div>
+    )
 }
 
 
